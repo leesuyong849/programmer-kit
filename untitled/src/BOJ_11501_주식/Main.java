ef2bc742dece0e63d87da321ce9d84cffbe7cad4
leesuyong849 <toosee1005@cau.ac.kr>
2026-01-23 20:56:40 +0900

    WIP

--- untitled/src/BOJ_11501_주식/Main.java
+++ untitled/src/BOJ_11501_주식/Main.java
@@ -1,10 +1,49 @@
 package BOJ_11501_주식;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static int T, N;
+    static int[] map;
+
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            map = new int[N];
+            for (int a = 0; a < N; a++) {
+                map[a] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int a = N - 1; a >= 0; a--) {
+
+            }
+        }
+    }
+
+    public static void teat() {
+        int[][] dp = new int[N][3];
+        int[][] dp_cnt = new int[N][3];
+        dp[0][0] -= map[0];
+        dp_cnt[0][0] = 1;
+
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+
+        dp_cnt[0][1] = 1;
+        dp_cnt[0][2] = 1;
+
+        for (int i = 1; i < N; i++) {
+
+        }
 
     }
 }
