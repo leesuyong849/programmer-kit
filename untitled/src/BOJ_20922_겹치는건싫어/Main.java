331059fd7472e97ba37c82d307ec97b39a208b65
leesuyong849 <toosee1005@cau.ac.kr>
2026-02-20 11:40:55 +0900

    BOJ[20922] 겹치는 건 싫어

---
+++ untitled/src/BOJ_20922_겹치는건싫어/Main.java
@@ -0,0 +1,44 @@
+package BOJ_20922_겹치는건싫어;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, K;
+    static int[] map;
+    static int[] cnt = new int[100001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+        int max = 0;
+
+        for (int right = 0; right < N; right++) {
+            int x = map[right];
+            cnt[x]++;
+
+            while (cnt[x] > K) {
+                cnt[map[left]]--;
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
+        }
+
+        System.out.println(max);
+    }
+}
