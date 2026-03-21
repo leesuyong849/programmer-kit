c5b7bc3693416f0ae8062b479280c77d3496d3d6
leesuyong849 <toosee1005@cau.ac.kr>
2026-03-21 22:30:19 +0900

    week18: 15989 123 더하기 4

--- untitled/src/BOJ_15989_123더하기4/Main2.java
+++ untitled/src/BOJ_15989_123더하기4/Main2.java
@@ -8,25 +8,27 @@ import java.util.StringTokenizer;
 public class Main2 {
 
     static int T, N;
-    static int[] map;
+    static int[][] map;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        T = Integer.parseInt(bf.readLine());
-        map = new int[100001];
-        map[1] = 1;
-        map[2] = 2;
 
+        T = Integer.parseInt(st.nextToken());
+        map = new int[10001][4];
 
-        for (int i = 0; i < T; i++) {
-            int max3 = (int) Math.floor( N / 3);
-            int cnt = 0;
-
-            for (int j = max3; j >= 0; j--) {
-
+        for(int t = 0; t<T; t++)
+        {
+            N = Integer.parseInt(br.readLine());
+            map[1][1] = map[2][1] = map[2][2] = map[3][1] = map[3][2] = map[3][3] = 1;
+            for(int i=4; i<=N; i++)  {
+                map[i][1] = map[i-1][1];
+                map[i][2] = map[i-2][1] + map[i-2][2];
+                map[i][3] = map[i-3][1] + map[i-3][2] + map[i-3][3];
             }
-
+            sb.append(map[N][3]+map[N][2]+map[N][1]+"\n");
         }
+        System.out.println(sb);
     }
 }
