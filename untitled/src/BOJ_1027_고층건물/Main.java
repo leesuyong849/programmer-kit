9a2f23cbfde2d8285426f7b0a39c797525e81053
leesuyong849 <toosee1005@cau.ac.kr>
2026-03-24 14:32:50 +0900

    week19: BOJ[1027] 고층 건물

---
+++ untitled/src/BOJ_1027_고층건물/Main.java
@@ -0,0 +1,56 @@
+package BOJ_1027_고층건물;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] map;
+    static int Max;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            int count = 0;
+
+            // 왼쪽 보기
+            double maxSlope = Double.NEGATIVE_INFINITY;
+            for (int j = i - 1; j >= 0; j--) {
+                double slope = (double) (map[j] - map[i]) / (j - i);
+                if (slope > maxSlope) {
+                    maxSlope = slope;
+                    count++;
+                }
+            }
+
+            // 오른쪽 보기
+            maxSlope = Double.NEGATIVE_INFINITY;
+            for (int j = i + 1; j < N; j++) {
+                double slope = (double) (map[j] - map[i]) / (j - i);
+                if (slope > maxSlope) {
+                    maxSlope = slope;
+                    count++;
+                }
+            }
+
+            answer = Math.max(answer, count);
+        }
+
+        System.out.println(answer);
+
+    }
+}
