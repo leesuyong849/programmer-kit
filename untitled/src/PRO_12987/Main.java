c78312e51aecda234396b354ceae77ba2feec935
leesuyong849 <toosee1005@cau.ac.kr>
2026-06-07 09:50:41 +0900

    PRO12987 [숫자 게임]

---
+++ untitled/src/PRO_12987/Main.java
@@ -0,0 +1,30 @@
+package PRO_12987;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int count = 0;
+        int i = 0; // A 포인터
+
+        for (int j = 0; j < B.length; j++) {
+            if (B[j] > A[i]) {
+                count++;
+                i++;
+            }
+        }
+
+        return count;
+    }
+}
