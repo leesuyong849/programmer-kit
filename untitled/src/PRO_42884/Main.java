58d9baa2f5f52639272bfb3e30000e618cd9ccd6
leesuyong849 <toosee1005@cau.ac.kr>
2026-07-10 13:17:44 +0900

    PROG[42884]

--- untitled/src/PRO_42884/Main.java
+++ untitled/src/PRO_42884/Main.java
@@ -12,7 +12,7 @@ public class Main {
         int camera = routes[0][1];  //처음으로 나가는 사람이 있는곳부터 단속 시작
 
         for (int i = 1; i < routes.length; i++) {
-            // 가장 최신 카메라가 단속 못하면 하나 더 추가
+            // 가장 최신 카메라가 단속 못하면 하나 더 추
             if (routes[i][0] > camera) {
                 count++;
                 camera = routes[i][1];
